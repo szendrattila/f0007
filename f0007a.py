31a600b2cf23568399fc15802400bcbbbb5d31b2
szendrattila <attila8000a@gmail.com>
2020-10-20 08:40:05 +0000

    f0007

--- f0007a.py
+++ f0007a.py
@@ -1,2 +1,4 @@
-a = 8
-print(a)
+kereszt = input('Mi a kereszt neved? ')
+vezeték= input('Mi a vezeték neved? ')
+kor = input('hány éves vagy? ')
+print('csá ', kereszt,' ',vezeték,  ' aki'' ',kor,' ' 'éves.' , sep='')
